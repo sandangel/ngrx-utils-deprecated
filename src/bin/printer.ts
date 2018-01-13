cb457691599000909247a681d14cc12836dfb2c2
sandangel <vinhsannguyen91@gmail.com>
2018-01-14 01:04:39 +0900

    fix: ensure union type name is valid identifier

--- src/bin/printer.ts
+++ src/bin/printer.ts
@@ -38,7 +38,7 @@ function parseActionType(type: string) {
   }
 
   return {
-    category: result[1] as string,
+    category: (result[1] as string).replace(/(\.|-)/, ''),
     name: result[2] as string
   };
 }
